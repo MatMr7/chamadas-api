93fba898b34e9be0c31a95effd97df402a44e0da
TH <ti.matheus.morais@gmail.com>
2023-11-04 13:09:32 -0300

    routes fix

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\Admin\AdminController;
 use App\Http\Controllers\Admin\AlunoController;
 use App\Http\Controllers\Admin\ProfessorController;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\Professor\TurmaController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -24,7 +25,10 @@ Route::middleware('auth:sanctum')->group(function () {
         Route::post('/', [AdminController::class, 'create']);
         Route::post('/professor', [ProfessorController::class, 'create']);
         Route::post('/aluno', [AlunoController::class, 'create']);
+    });
 
+    Route::prefix('professor')->group(function () {
+        Route::post('/turma', [TurmaController::class, 'create']);
     });
 });
 
