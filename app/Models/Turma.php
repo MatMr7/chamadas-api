247869b13bf498ed9f9205b3caac57afd6ea4366
TH <ti.matheus.morais@gmail.com>
2023-11-04 13:08:23 -0300

    Rota para criar turma

--- app/Models/Turma.php
+++ app/Models/Turma.php
@@ -5,6 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property int $professor_id
+ * @property string $nome
+ * @property int $disciplina_id
+ */
 class Turma extends Model
 {
     use HasFactory;
