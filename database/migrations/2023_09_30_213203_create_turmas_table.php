564f5b454734ba25337c5df5ab8923ecc60d9ef2
Matheus Morais <51454044+MatMr7@users.noreply.github.com>
2023-11-23 17:16:20 -0300

    Rota para criar turma (#4)

--- database/migrations/2023_09_30_213203_create_turmas_table.php
+++ database/migrations/2023_09_30_213203_create_turmas_table.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\Disciplina;
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,6 +15,9 @@ return new class extends Migration
     {
         Schema::create('turmas', function (Blueprint $table) {
             $table->id();
+            $table->string('nome');
+            $table->foreignIdFor(User::class, 'professor_id');
+            $table->foreignIdFor(Disciplina::class, 'disciplina_id');
             $table->timestamps();
         });
     }
